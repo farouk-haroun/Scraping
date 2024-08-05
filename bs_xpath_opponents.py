fe7083afae88e6c8a3383cad8933ccccaae1f270
Haroun <farouk.haroun@vanderbilt.edu>
2024-08-05 14:25:38 -0500

    xpath

---
+++ bs_xpath_opponents.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+from parsel import Selector
+
+def get_opponents(html):
+    selector = Selector(text = html )
+    tables = soup.select('table[class = "wikitable"]')
+
+    matches = tables[0]
+    trs = matches.select("tr")
+    opponents = []
+    links = [] 
+    for tr in trs:
+        opponent = {}
+        opponent_node = tr.select_one("td:nth-child(3)")
+        
+        if not opponent_node:
+            continue
+        
+        opponent_name = opponent_node.string
+        opponent_link = None 
+        if not opponent_name:
+            opponent_name = opponent_node.select_one("a").string
+            opponent_link = f"https://en.wikipedia.org/{opponent_node.select_one("a").get('href')}"
+            
+            
+        opponent['name'] =  opponent_name.strip('\n')
+        opponent['link'] = opponent_link
+        opponents.append(opponent)
+    
+    return opponents
